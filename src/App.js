81832bde6282da28c31a567a50c8efc645399bbc
Imogen Hare <imogenbhare@gmail.com>
2022-07-11 10:18:50 +0100

    front end started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { ConnectButton } from "web3uikit";
+import logo from "./images/Moralis.png";
+
 
 
 const App = () => {
 
   return (
     <>
+    <div className="header">
+      <div className="logo">
+        <img src={logo} alt="logo" height="50px"/>
+          Sentiment
+      </div>
+      <ConnectButton/>
+    </div>
+    <div className="instructions">
+      Who do you think will win Wimbledon?
+    </div>
+
      
     </>
   );
